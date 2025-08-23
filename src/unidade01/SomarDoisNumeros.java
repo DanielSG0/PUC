cac03631cacc108f54e137e36c105e2cd243e7ce
Daniel Santos Garcia <danielsantosgarcia0@gmail.com>
2025-08-22 22:27:19 -0300

    Exercicio 1 finalizado

--- src/unidade01/SomarDoisNumeros.java
+++ src/unidade01/SomarDoisNumeros.java
@@ -1,5 +1,20 @@
 package unidade01;
+import java.util.*;
+
+//Daniel Santos Garcia - EX01 - 1598779
 
 class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
 	
+	
+	public static void main(String args[]) {
+		System.out.println("Digite o primeiro número: ");
+		int num1 = sc.nextInt();
+		System.out.println("Digite o segundo número: ");
+		int num2 = sc.nextInt();
+		
+		int soma = num1+num2;
+		
+		System.out.println("O valor da soma é: " + soma);
+	}
 }
